a61b92e1f8bccd18e47ddfe8e62c1e0b116e24b9
SukMin Lee <s2019s16@e-mirim.hs.kr>
2021-04-11 11:15:25 +0900

    Starlight village completion

---
+++ Assets/Scenes/Map/FullMap1/FullMap1.cs
@@ -0,0 +1,117 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FullMap1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void f1()
+    {
+        SceneManager.LoadScene("FullMap1-1");
+    }
+    public void f2()
+    {
+        SceneManager.LoadScene("FullMap1-2");
+    }
+    public void f3()
+    {
+        SceneManager.LoadScene("FullMap1-3");
+    }
+    public void f4()
+    {
+        SceneManager.LoadScene("FullMap1-4");
+    }
+    public void f5()
+    {
+        SceneManager.LoadScene("FullMap1-5");
+    }
+    public void f6()
+    {
+        SceneManager.LoadScene("FullMap1-6");
+    }
+    public void f7()
+    {
+        SceneManager.LoadScene("FullMap1-7");
+    }
+    public void f8()
+    {
+        SceneManager.LoadScene("FullMap1-8");
+    }
+    public void f9()
+    {
+        SceneManager.LoadScene("FullMap1-9");
+    }
+    public void f11()
+    {
+        SceneManager.LoadScene("FullMap1-11");
+    }
+    public void f12()
+    {
+        SceneManager.LoadScene("FullMap1-12");
+    }
+    public void f13()
+    {
+        SceneManager.LoadScene("FullMap1-13");
+    }
+    public void f14()
+    {
+        SceneManager.LoadScene("FullMap1-14");
+    }
+    public void f15()
+    {
+        SceneManager.LoadScene("FullMap1-15");
+    }
+    public void f17()
+    {
+        SceneManager.LoadScene("FullMap1-17");
+    }
+    public void f18()
+    {
+        SceneManager.LoadScene("FullMap1-18");
+    }
+    public void f19()
+    {
+        SceneManager.LoadScene("FullMap1-19");
+    }
+    public void f20()
+    {
+        SceneManager.LoadScene("FullMap1-20");
+    }
+    public void f21()
+    {
+        SceneManager.LoadScene("FullMap1-21");
+    }
+    public void f22()
+    {
+        SceneManager.LoadScene("FullMap1-22");
+    }
+    public void f26()
+    {
+        SceneManager.LoadScene("FullMap1-26");
+    }
+    public void f27()
+    {
+        SceneManager.LoadScene("FullMap1-27");
+    }
+    public void f28()
+    {
+        SceneManager.LoadScene("FullMap1-28");
+    }
+    public void f29()
+    {
+        SceneManager.LoadScene("FullMap1-29");
+    }
+}
+
---
+++ Assets/Scenes/Map/FullMap6/FullMap6.cs
@@ -0,0 +1,115 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FullMap6 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void f1()
+    {
+        SceneManager.LoadScene("FullMap6-1");
+    }
+    public void f2()
+    {
+        SceneManager.LoadScene("FullMap6-2");
+    }
+    public void f3()
+    {
+        SceneManager.LoadScene("FullMap6-3");
+    }
+    public void f4()
+    {
+        SceneManager.LoadScene("FullMap6-4");
+    }
+    public void f5()
+    {
+        SceneManager.LoadScene("FullMap6-5");
+    }
+    public void f6()
+    {
+        SceneManager.LoadScene("FullMap6-6");
+    }
+    public void f7()
+    {
+        SceneManager.LoadScene("FullMap6-7");
+    }
+    public void f8()
+    {
+        SceneManager.LoadScene("FullMap6-8");
+    }
+    public void f9()
+    {
+        SceneManager.LoadScene("FullMap6-9");
+    }
+    public void f11()
+    {
+        SceneManager.LoadScene("FullMap6-11");
+    }
+    public void f12()
+    {
+        SceneManager.LoadScene("FullMap6-12");
+    }
+    public void f13()
+    {
+        SceneManager.LoadScene("FullMap6-13");
+    }
+    public void f14()
+    {
+        SceneManager.LoadScene("FullMap6-14");
+    }
+    public void f15()
+    {
+        SceneManager.LoadScene("FullMap6-15");
+    }
+    public void f17()
+    {
+        SceneManager.LoadScene("FullMap6-17");
+    }
+    public void f18()
+    {
+        SceneManager.LoadScene("FullMap6-18");
+    }
+    public void f19()
+    {
+        SceneManager.LoadScene("FullMap6-19");
+    }
+    public void f20()
+    {
+        SceneManager.LoadScene("FullMap6-20");
+    }
+    public void f21()
+    {
+        SceneManager.LoadScene("FullMap6-21");
+    }
+    public void f22()
+    {
+        SceneManager.LoadScene("FullMap6-22");
+    }
+    public void f26()
+    {
+        SceneManager.LoadScene("FullMap6-26");
+    }
+    public void f27()
+    {
+        SceneManager.LoadScene("FullMap6-27");
+    }
+    public void f28()
+    {
+        SceneManager.LoadScene("FullMap6-28");
+    }
+    public void f29()
+    {
+        SceneManager.LoadScene("FullMap6-29");
+    }
+}
